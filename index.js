1bacd6e3a881fe0721508e75d1cf4e417448ed5d
Ron Almog <ron8132@gmail.com>
2021-11-02 11:49:09 -0400

    initial state, action, reducer

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("from index.js");
+const BUY_CAKE = "BUY_CAKE";
+
+// action creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "first action",
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
